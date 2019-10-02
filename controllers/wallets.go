4ba603185c1ef977a276c2acd20a27569d9a1dc9
Richard B <eros@polispay.org>
2019-10-02 00:46:14 -0500

    Unfuckup

--- controllers/wallets.go
+++ controllers/wallets.go
@@ -29,7 +29,7 @@ type WalletController struct{}
 func (w *WalletController) GetInfo(c *gin.Context) {
 	_, err := utils.VerifyHeaderSignature(c)
 	if err != nil {
-		res.GlobalResponseError(nil, err, c)
+		res.GlobalResponseNoAuth(c)
 		return
 	}
 	coin := c.Param("coin")
