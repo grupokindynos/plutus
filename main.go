9a0d88a4571903c84e2615def519fa40d7100d8a
Enrique <eabz@polispay.org>
2019-12-09 19:52:31 -0600

    Enable validations

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/grupokindynos/common/responses"
+	"github.com/grupokindynos/common/tokens/mrt"
+	"github.com/grupokindynos/common/tokens/mvt"
 	"github.com/grupokindynos/plutus/controllers"
 	_ "github.com/heroku/x/hmetrics/onload"
 	_ "github.com/joho/godotenv/autoload"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
@@ -48,28 +49,27 @@ func ApplyRoutes(r *gin.Engine) {
 }
 
 func VerifyRequest(c *gin.Context, method func(params controllers.Params) (interface{}, error)) {
-	/*payload, err := mvt.VerifyRequest(c)
+	payload, err := mvt.VerifyRequest(c)
 	if err != nil {
 		responses.GlobalResponseNoAuth(c)
 		return
-	}*/
-	body, err := ioutil.ReadAll(c.Request.Body)
+	}
 	params := controllers.Params{
 		Coin: c.Param("coin"),
 		Txid: c.Param("txid"),
-		Body: body,
+		Body: payload,
 	}
 	response, err := method(params)
 	responses.GlobalResponseError(response, err, c)
-	/*	if err != nil {
-		responses.GlobalResponseError(nil, err, c)
-		return
-	}*/
-	/*header, body, err := mrt.CreateMRTToken("plutus", os.Getenv("MASTER_PASSWORD"), response, os.Getenv("PLUTUS_PRIVATE_KEY"))
 	if err != nil {
 		responses.GlobalResponseError(nil, err, c)
 		return
 	}
-	responses.GlobalResponseMRT(header, body, c)*/
+	header, body, err := mrt.CreateMRTToken("plutus", os.Getenv("MASTER_PASSWORD"), response, os.Getenv("PLUTUS_PRIVATE_KEY"))
+	if err != nil {
+		responses.GlobalResponseError(nil, err, c)
+		return
+	}
+	responses.GlobalResponseMRT(header, body, c)
 	return
 }
